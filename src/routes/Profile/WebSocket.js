41f0255232373afda1dea0a9127a3935a6c1612e
xuehaipeng <xuehaipeng@jd.com>
2018-03-21 14:18:53 +0800

    add websocket subscription

--- src/routes/Profile/WebSocket.js
+++ src/routes/Profile/WebSocket.js
@@ -7,9 +7,19 @@ import SockJsClient from 'react-stomp';
 const { Description } = DescriptionList;
 
 export default class UserCenter extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    }
+  }
   sendMessage = (msg) => {
     this.clientRef.sendMessage('/topic/process', msg);
+  }
+  updateCount = (msg) => {
+    this.setState({
+      count: msg
+    })
   }
 
   render() {
@@ -18,9 +28,17 @@ export default class UserCenter extends Component {
         <Card title="用户信息" style={{ marginBottom: 24 }} bordered={false}>
           <div>
             <SockJsClient url='http://localhost:8080/ws' topics={['/topic/process']}
-                          onMessage={(msg) => { console.log(msg); }}
+                          onMessage={this.updateCount}
                           ref={ (client) => { this.clientRef = client }} />
           </div>
+          <DescriptionList style={{ marginBottom: 24 }}>
+            <Description term="用户姓名">曲丽丽</Description>
+            <Description term="会员卡号">32943898021309809423</Description>
+            <Description term="身份证">3321944288191034921</Description>
+            <Description term="联系方式">18112345678</Description>
+            <Description term="当前计数">{this.state.count.toString()}</Description>
+            <Description term="职位">前端交互设计师</Description>
+          </DescriptionList>
         </Card>
       </Fragment>
     );
