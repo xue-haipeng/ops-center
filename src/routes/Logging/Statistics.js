a2f28cfc8502db203fe1d027350d3bc40cfea6ab
xuehaipeng <1071405234@qq.com>
2018-11-18 10:32:09 +0800

    sync to github

--- src/routes/Logging/Statistics.js
+++ src/routes/Logging/Statistics.js
@@ -94,284 +94,6 @@ export default class Statistics extends Component {
         item.y];
     });
 
-// eslint-disable-next-line prefer-destructuring
-    const colors = Highcharts.getOptions().colors;
-    const categories = [
-            "Chrome",
-            "Firefox",
-            "Internet Explorer",
-            "Safari",
-            "Edge",
-            "Opera",
-            "Other",
-          ];
-    const data = [
-            {
-              "y": 62.74,
-              "color": colors[2],
-              "drilldown": {
-                "name": "Chrome",
-                "categories": [
-                  "Chrome v65.0",
-                  "Chrome v64.0",
-                  "Chrome v63.0",
-                  "Chrome v62.0",
-                  "Chrome v61.0",
-                  "Chrome v60.0",
-                  "Chrome v59.0",
-                  "Chrome v58.0",
-                  "Chrome v57.0",
-                  "Chrome v56.0",
-                  "Chrome v55.0",
-                  "Chrome v54.0",
-                  "Chrome v51.0",
-                  "Chrome v49.0",
-                  "Chrome v48.0",
-                  "Chrome v47.0",
-                  "Chrome v43.0",
-                  "Chrome v29.0",
-                ],
-                "data": [
-                  0.1,
-                  1.3,
-                  53.02,
-                  1.4,
-                  0.88,
-                  0.56,
-                  0.45,
-                  0.49,
-                  0.32,
-                  0.29,
-                  0.79,
-                  0.18,
-                  0.13,
-                  2.16,
-                  0.13,
-                  0.11,
-                  0.17,
-                  0.26,
-                ],
-              },
-            },
-            {
-              "y": 10.57,
-              "color": colors[1],
-              "drilldown": {
-                "name": "Firefox",
-                "categories": [
-                  "Firefox v58.0",
-                  "Firefox v57.0",
-                  "Firefox v56.0",
-                  "Firefox v55.0",
-                  "Firefox v54.0",
-                  "Firefox v52.0",
-                  "Firefox v51.0",
-                  "Firefox v50.0",
-                  "Firefox v48.0",
-                  "Firefox v47.0",
-                ],
-                "data": [
-                  1.02,
-                  7.36,
-                  0.35,
-                  0.11,
-                  0.1,
-                  0.95,
-                  0.15,
-                  0.1,
-                  0.31,
-                  0.12,
-                ],
-              },
-            },
-            {
-              "y": 7.23,
-              "color": colors[0],
-              "drilldown": {
-                "name": "Internet Explorer",
-                "categories": [
-                  "Internet Explorer v11.0",
-                  "Internet Explorer v10.0",
-                  "Internet Explorer v9.0",
-                  "Internet Explorer v8.0",
-                ],
-                "data": [
-                  6.2,
-                  0.29,
-                  0.27,
-                  0.47,
-                ],
-              },
-            },
-            {
-              "y": 5.58,
-              "color": colors[3],
-              "drilldown": {
-                "name": "Safari",
-                "categories": [
-                  "Safari v11.0",
-                  "Safari v10.1",
-                  "Safari v10.0",
-                  "Safari v9.1",
-                  "Safari v9.0",
-                  "Safari v5.1",
-                ],
-                "data": [
-                  3.39,
-                  0.96,
-                  0.36,
-                  0.54,
-                  0.13,
-                  0.2,
-                ],
-              },
-            },
-            {
-              "y": 4.02,
-              "color": colors[5],
-              "drilldown": {
-                "name": "Edge",
-                "categories": [
-                  "Edge v16",
-                  "Edge v15",
-                  "Edge v14",
-                  "Edge v13",
-                ],
-                "data": [
-                  2.6,
-                  0.92,
-                  0.4,
-                  0.1,
-                ],
-              },
-            },
-            {
-              "y": 1.92,
-              "color": colors[4],
-              "drilldown": {
-                "name": "Opera",
-                "categories": [
-                  "Opera v50.0",
-                  "Opera v49.0",
-                  "Opera v12.1",
-                ],
-                "data": [
-                  0.96,
-                  0.82,
-                  0.14,
-                ],
-              },
-            },
-            {
-              "y": 7.62,
-              "color": colors[6],
-              "drilldown": {
-                "name": 'Other',
-                "categories": [
-                  'Other',
-                ],
-                "data": [
-                  7.62,
-                ],
-              },
-            },
-          ];
-    const browserData = [];
-    const versionsData = [];
-    let i;
-    let j;
-    const dataLen = data.length;
-    let drillDataLen;
-    let brightness;
-// Build the data arrays
-    for (i = 0; i < dataLen; i += 1) {
-      // add browser data
-      browserData.push({
-        name: categories[i],
-        y: data[i].y,
-        color: data[i].color,
-      });
-      // add version data
-      drillDataLen = data[i].drilldown.data.length;
-      for (j = 0; j < drillDataLen; j += 1) {
-        brightness = 0.2 - (j / drillDataLen) / 5;
-        versionsData.push({
-          name: data[i].drilldown.categories[j],
-          y: data[i].drilldown.data[j],
-          color: Highcharts.Color(data[i].color).brighten(brightness).get(),
-        });
-      }
-    }
-
-    const options = {
-      chart: {
-        type: 'pie',
-      },
-      title: {
-        text: '',
-      },
-      subtitle: {
-        text: '',
-      },
-      yAxis: {
-        title: {
-          text: 'Total percent market share',
-        },
-      },
-      credits: {
-        enabled: false,
-      },
-      plotOptions: {
-        pie: {
-          shadow: false,
-          center: ['50%', '50%'],
-        },
-      },
-      tooltip: {
-        valueSuffix: '%',
-      },
-      series: [{
-        name: 'Browsers',
-        data: browserData,
-        size: '60%',
-        dataLabels: {
-          formatter () {
-            return this.y > 5 ? this.point.name : null;
-          },
-          color: '#ffffff',
-          distance: -30,
-        },
-      }, {
-        name: 'Versions',
-        data: versionsData,
-        size: '80%',
-        innerSize: '60%',
-        dataLabels: {
-          formatter () {
-            // display only if larger than 1
-            return this.y > 1 ? `<b>${  this.point.name  }:</b> ${ 
-              this.y  }%` : null;
-          },
-        },
-        id: 'versions',
-      }],
-      responsive: {
-        rules: [{
-          condition: {
-            maxWidth: 400,
-          },
-          chartOptions: {
-            series: [{
-              id: 'versions',
-              dataLabels: {
-                enabled: false,
-              },
-            }],
-          },
-        }],
-      },
-    };
-
     return (
       <Fragment>
         <Card
@@ -382,17 +104,32 @@ export default class Statistics extends Component {
           bodyStyle={{ padding: '20px' }}
           style={{ background: 'transparent' }}
         >
+          {beaCodeDistr && bizLineDistr && (
+            <Row gutter={24} style={{ paddingBottom: 24 }}>
+              <Col xl={12} lg={24} md={24} sm={24} xs={24}>
+                <Pie
+                  hasLegend
+                  subTitle="BEA Code分布"
+                  style={{ background: '#ffffff' }}
+                  total={beaCodeDistr.map(item => item.y).reduce((prev, curr) => prev + curr, 0)}
+                  data={beaCodeDistr}
+                  height={180}
+                />
+              </Col>
+              <Col xl={12} lg={24} md={24} sm={24} xs={24}>
+                <Pie
+                  hasLegend
+                  title="业务域分布"
+                  subTitle="业务域分布"
+                  style={{ background: '#ffffff' }}
+                  total={bizLineDistr.map(item => item.y).reduce((prev, curr) => prev + curr, 0)}
+                  data={bizLineDistr}
+                  height={180}
+                />
+              </Col>
+            </Row>
+          )}
           <Row gutter={24} style={{ paddingBottom: 24 }}>
-            <Col xl={12} lg={24} md={24} sm={24} xs={24}>
-              <Pie
-                hasLegend
-                subTitle="BEA Code分布"
-                style={{ background: '#ffffff' }}
-                total={beaCodeDistr.map(item => item.y).reduce((prev, curr) => prev + curr, 0)}
-                data={beaCodeDistr}
-                height={180}
-              />
-            </Col>
             <Col xl={12} lg={24} md={24} sm={24} xs={24}>
               <Pie
                 hasLegend
@@ -401,19 +138,6 @@ export default class Statistics extends Component {
                 style={{ background: '#ffffff' }}
                 total={userIdDistr.map(item => item.y).reduce((prev, curr) => prev + curr, 0)}
                 data={userIdDistr}
-                height={180}
-              />
-            </Col>
-          </Row>
-          <Row gutter={24} style={{ paddingBottom: 24 }}>
-            <Col xl={12} lg={24} md={24} sm={24} xs={24}>
-              <Pie
-                hasLegend
-                title="业务域分布"
-                subTitle="业务域分布"
-                style={{ background: '#ffffff' }}
-                total={bizLineDistr.map(item => item.y).reduce((prev, curr) => prev + curr, 0)}
-                data={bizLineDistr}
                 height={180}
               />
             </Col>
